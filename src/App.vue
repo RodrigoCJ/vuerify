e6a1d5cb107356f12146629bf5faf193b19d38c0
Rodrigo <rodrigocj>
2021-12-28 16:03:11 -0300

    cep

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-app-bar app color="primary" dark> 
       <v-app-bar-title>Vue+ Vuetify</v-app-bar-title>
       <v-spacer></v-spacer>
-      <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2" >{{ link }}</v-btn>
+      <v-btn v-for="(link, i) in links" :key="i" color="white" text rounded class="my-2" :to="link.path">{{ link.label}} </v-btn>
     </v-app-bar>
 
     <!-- Login -->
@@ -12,7 +12,7 @@
     </v-main>
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
-        <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2" > {{ link }} </v-btn>
+        <v-btn v-for="(link, i) in links" :key="i" color="white" text rounded class="my-2" :to="link.path"> {{ link.label }} </v-btn>
         <v-col class="primary lighten-2 py-4 text-center white--text" cols="12" > {{ new Date().getFullYear() }} — <strong>Solinftec</strong> </v-col>
       </v-row>
     </v-footer>
@@ -24,7 +24,19 @@
 export default {
   name: 'App',
   data: () => ({
-    links:["Home", "Login","Cadastro"]
+    links:[
+      {
+        label:"Home",
+        path:"/"
+      },
+      {
+        label:"Login",
+        path:"/login"
+      },
+      {
+        label:"About",
+        path:"/about"
+      }]
   }),
 };
 </script>
