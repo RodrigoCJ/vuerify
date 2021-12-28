6b1d6e99cd5ae5773ee4eb78b2fd0894fc40d212
Rodrigo <rodrigocj>
2021-12-28 12:01:36 -0300

    add router, mudado login

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,12 @@
 
     <!-- Login -->
     <v-main>
-      <v-card width="500" class="mx-auto mt-5">
-        <v-card-title>Login</v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle"></v-text-field>
-            <v-text-field label="Password" prepend-icon="mdi-lock" :append-icon="exibir ? 'mdi-eye' : 'mdi-eye-off'" :type="exibir ? 'text' : 'password'" @click:append="() => (exibir = !exibir)"></v-text-field>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-main>
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
         <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2" > {{ link }} </v-btn>
-        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12" > {{ new Date().getFullYear() }} — <strong>Vuetify</strong> </v-col>
+        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12" > {{ new Date().getFullYear() }} — <strong>Solinftec</strong> </v-col>
       </v-row>
     </v-footer>
   </v-app>
@@ -36,12 +23,7 @@
 
 export default {
   name: 'App',
-
-  components: {
-  },
-
   data: () => ({
-    exibir: false,
     links:["Home", "Login","Cadastro"]
   }),
 };
